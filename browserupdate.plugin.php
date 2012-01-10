40a29fa43d8b3015d43f25a5b63c735720670465
Konzertheld <christian.gredig@googlemail.com>
2012-01-10 02:11:50 +0100

    Add an option to define which browser versions should be defined as "too old". Not functional, yet, as the user can't enter anything.

--- browserupdate.plugin.php
+++ browserupdate.plugin.php
@@ -21,7 +21,12 @@ class Browserupdate extends Plugin
 	 **/
 	public function action_plugin_ui_configure($plugin_id, $action)
 	{
-		
+		$ui = new FormUI(strtolower(get_class($this)));
+		$ui->append('checkbox', 'use_userbrowserversions', __CLASS__ . '__use_userbrowserversions', _t('Do not automatically set browser version depending on what the publisher still supports', __CLASS__));
+		$ui->append('hidden', 'userbrowserversions', __CLASS__ . '__userbrowserversions');
+		$ui->set_option('success_message', _t('Options saved'));
+		$ui->append('submit', 'save', _t('Save', __CLASS__ ));
+		$ui->out();
 	}
 	
 	/**
@@ -29,7 +34,18 @@ class Browserupdate extends Plugin
 	 **/
 	public function action_template_header()
 	{
-		Stack::add('template_header_javascript', $this->get_url(true) . 'browserupdate.js', 'browserupdate');
+		$opts = Options::get_group( __CLASS__ );
+		if($opts['use_userbrowserversions'])
+		{
+			// Use the browser versions the user wanted
+			Stack::add('template_header_javascript', "var \$buoop = {" . $opts['userbrowserversions'] . "};", 'browserupdate_browsers');
+		}
+		else
+		{
+			// Just use the automatic way
+			Stack::add('template_header_javascript', "var \$buoop = {};", 'browserupdate_browsers');
+		}
+		Stack::add('template_header_javascript', $this->get_url(true) . 'browserupdate.js', 'browserupdate', 'browserupdate_browsers');
 	}
 }
 ?>
